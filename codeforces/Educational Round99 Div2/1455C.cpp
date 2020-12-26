14d54d5a9658f4326e0360b28fa3be8dca39e843
Evelyn <54436228+Evelyn82@users.noreply.github.com>
2020-12-26 21:07:18 +0900

    Ping Pong

---
+++ codeforces/Educational Round99 Div2/1455C.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	
+	int t; cin>>t;
+	while(t--){
+		int x, y;
+		cin>>x>>y;
+		cout<<x-1<<' '<<y<<'\n';
+				
+		/*
+    WA code
+		if(x == y) cout<<0<<' '<<1<<'\n';
+		else if(x > y) cout<<x-y<<' '<<1<<'\n';
+		else cout<<0<<' '<<y-x+1<<'\n';
+		*/
+	}
+}

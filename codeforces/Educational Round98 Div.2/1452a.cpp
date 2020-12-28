11fa20a4575d175a7e6f6e6fee10a5d44842f2df
Evelyn <54436228+Evelyn82@users.noreply.github.com>
2020-12-28 10:36:53 +0900

    Robot Program

---
+++ codeforces/Educational Round98 Div.2/1452a.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+ 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	int t; cin >> t;
+	while (t--) {
+		int x, y; cin >> x >> y;
+ 
+		int sub = min(x, y), h = max(x, y), ans = 0;
+		if (sub == 0 && h > 0) ans = (h - 2) * 2 + 3;
+		else {
+			ans = sub * 2;
+			h -= sub;
+			if (h) ans += (h - 1) * 2 + 1;
+		}cout << ans << '\n';
+	}
+}

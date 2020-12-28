bc7f67c46de43b5b353940a76a1ef476f905112d
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2020-12-28 16:25:44 +0900

    Update 1452a.cpp

--- codeforces/Educational Round98 Div.2/1452a.cpp
+++ codeforces/Educational Round98 Div.2/1452a.cpp
@@ -10,11 +10,23 @@ int main() {
 		int x, y; cin >> x >> y;
  
 		int sub = min(x, y), h = max(x, y), ans = 0;
+		// 15ms
 		if (sub == 0 && h > 0) ans = (h - 2) * 2 + 3;
 		else {
 			ans = sub * 2;
 			h -= sub;
 			if (h) ans += (h - 1) * 2 + 1;
 		}cout << ans << '\n';
+		
+		
+		/*
+		31ms
+	        if(sub == h) ans = sub * 2;
+        	else {
+            		ans = sub * 2;
+           		h -= sub;
+            		ans += (h - 1) * 2 + 1;
+        	}cout << ans << '\n';
+		*/
 	}
 }

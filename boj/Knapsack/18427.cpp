89f5b24394077bced964e048b05c48ff4e933750
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2020-12-30 14:15:41 +0900

    Create 18427.cpp

---
+++ boj/Knapsack/18427.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+
+int d[1001];
+vector<vector<int>> bcks;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int N,M,H; cin>>N>>M>>H;
+    cin.ignore();
+    bcks.resize(N);
+    for(int i=0;i<N;++i){
+        string s;
+        getline(cin, s);
+        
+        int num=0;
+        for(int j=0;j<=s.size();++j){
+            if(j==s.size()) bcks[i].push_back(num);
+            else if(s[j]==' ') {
+                bcks[i].push_back(num);
+                num=0;
+            }
+            else num=(num*10)+(int)(s[j]-'0');
+        }
+    }
+    
+    d[0]=1; // start point
+    
+    for(int n=0;n<N;++n){
+        vector<pair<int, int>> v;
+        for(int h=0;h<=H;++h){
+            for(int b=0;b<bcks[n].size();++b){
+                if(d[h]!=0 && h+bcks[n][b]<=H){
+                    // Push all cases
+                    v.push_back(make_pair(h+bcks[n][b], d[h]));
+                }
+            }
+        }
+        for(pair<int,int>& p : v){
+            d[p.first]=(d[p.first]+p.second)%10007;
+        }
+    }
+    cout<<d[H]<<'\n';
+    return 0;
+}

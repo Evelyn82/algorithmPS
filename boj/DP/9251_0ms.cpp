8f95a1d865e58f8fa9f86350cac605762fdc0eb2
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-01-06 23:05:24 +0900

    LCS

---
+++ boj/DP/9251_0ms.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+const int MAXN=1e3+1;
+int d[2][MAXN];
+char a[MAXN], b[MAXN];
+int main(){
+    scanf("%s %s", a+1, b+1);
+    
+    int j, idx=0;
+    for(int i=1;a[i];++i){
+        for(j=1;b[j];++j){
+            if(a[i]==b[j]) d[idx][j]=d[idx^1][j-1]+1;
+            else d[idx][j]=max(d[idx][j-1], d[idx^1][j]);
+        }
+        idx^=1;
+    }
+    printf("%d\n", d[idx^1][j-1]);
+}

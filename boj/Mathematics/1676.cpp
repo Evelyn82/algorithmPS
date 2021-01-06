9d41b4ea0033de9a84eedf6d0eada312b6e9b4a3
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-01-06 17:15:50 +0900

    Update 1676.cpp

--- boj/Mathematics/1676.cpp
+++ boj/Mathematics/1676.cpp
@@ -14,11 +14,18 @@ int main(){
     ll n; cin>>n;
     
     ll five=0;
+    /*
     for(int i=1;i<=n;++i){
         five+=divide(i, 5);
         
         // div 5만 해도 되는 이유 -> 소인수 2의 개수가 5보다 훨씬 많아서 고려 안 해도 된다.
         //two+=divide(i,2);
+    }
+    */
+    
+    // 25(5^2), 125(5^3), 625(5^4)... 5의 거듭제곱부분에서 +1씩 해주는 방법
+    for(int i=5;i<=n;i*=5){
+        five+=n/i;
     }
     cout<<five<<'\n';
 }

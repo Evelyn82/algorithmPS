af98b4ebaa8b83b1b4d3568df7affbb456f0155d
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-01-10 21:22:53 +0900

    Update 11658_148ms.cpp

--- boj/Segment Tree/11658_148ms.cpp
+++ boj/Segment Tree/11658_148ms.cpp
@@ -2,30 +2,28 @@
 using namespace std;
 typedef long long ll;
 
-const int MX=1024;
-int arr[MX+1][MX+1], fw[MX+1][MX+1];
+const int MX=1025;
+int arr[MX][MX], fw[MX][MX];
 int n;
-void build()
-{
+void initialSet(){
 	for(int x=1;x<=n;++x)
 		for(int y=1;y<=n;++y){
 			int y2=y+(y&-y);
 			if(y2<=n) fw[x][y2]+=fw[x][y];
 		}
+	
 	for(int y=1;y<=n;++y)
 		for(int x=1;x<=n;++x){
 			int x2=x+(x&-x);
 			if(x2<=n) fw[x2][y]+=fw[x][y];
 		}
 }
-void update(int x,int y,int num)
-{
+void update(int x,int y,int num){
 	for(;x<=n;x+=x&-x)
 		for(int y1=y;y1<=n;y1+=y1&-y1)
 			fw[x][y1]+=num;
 }
-int query(int x,int y)
-{
+int query(int x,int y){
 	int sum=0;
 	for(;x>0;x-=x&-x)
 		for(int y1=y;y1>0;y1-=y1&-y1)
@@ -34,27 +32,27 @@ int query(int x,int y)
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); cout.tie(nullptr);
-	  int m; cin>>n>>m;
-	  for(int i=1;i<=n;++i) for(int j=1;j<=n;++j) {cin>>arr[i][j]; fw[i][j]=arr[i][j];}
-  	build();
+	ios_base::sync_with_stdio(false);
+    	cin.tie(nullptr); cout.tie(nullptr);
+	int m; cin>>n>>m;
+	for(int i=1;i<=n;++i) for(int j=1;j<=n;++j) {cin>>arr[i][j]; fw[i][j]=arr[i][j];}
+  	initialSet();
 	
-	  int w, x, y, x2, y2;
+	int w, x, y, x2, y2;
   	while(m--){
-      cin>>w>>x>>y;
-		  if(w){
-		  	cin>>x2>>y2;
-       	int ret=query(x2,y2);
-        ret-=query(x2,y-1);
-        ret-=query(x-1,y2);
-        ret+=query(x-1,y-1);
-        cout<<ret<<'\n';
-		  }
-		  else{
-			  cin>>x2;
-			  update(x,y,x2-arr[x][y]);
-			  arr[x][y]=x2;
+		cin>>w>>x>>y;
+		if(w){
+			cin>>x2>>y2;
+			int ret=query(x2,y2);
+		        ret-=query(x2,y-1);
+        		ret-=query(x-1,y2);
+        		ret+=query(x-1,y-1);
+       			cout<<ret<<'\n';
+		}
+		else{
+			cin>>x2;
+			update(x,y,x2-arr[x][y]);
+			arr[x][y]=x2;
 	  	}
-	  }
+	}
 }

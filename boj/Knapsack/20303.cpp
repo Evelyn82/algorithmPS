f5635b100d0b66b393f4facd4557c14f7903369d
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-01-10 19:09:05 +0900

    Update 20303.cpp

--- boj/Knapsack/20303.cpp
+++ boj/Knapsack/20303.cpp
@@ -3,7 +3,62 @@
 #include <algorithm>
 using namespace std;
 using pii = pair<int, int>;
+const int MAXN = 3E4 + 1;
 
+
+/*
+
+92ms, 5672KB
+
+vector<int> adj[MAXN];
+int candy[MAXN], dp[MAXN], grp;
+bool visited[MAXN];
+pii d[MAXN];
+
+void dfs(int n){
+	visited[n]=true;
+	d[grp].first++;
+	d[grp].second+=candy[n];
+	
+	for(int& i : adj[n]) if(!visited[i]) dfs(i);
+}
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	int n,m,k; cin>>n>>m>>k;
+	for(int i=1;i<=n;++i) cin>>candy[i];
+	
+	int a, b;
+	for(int i=0;i<m;++i){
+		cin>>a>>b;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	
+	grp=0;
+	memset(visited, false, sizeof(visited));
+	for(int i=1;i<=n;++i){
+		if(!visited[i]) {
+			dfs(i);
+			grp++;
+		}
+	}
+	
+	for(int i=0;i<grp;++i){
+		for(int j=k-1;j>=0;--j){
+			if(d[i].first>j) continue;
+			dp[j]=max(dp[j], dp[j-d[i].first]+d[i].second);
+		}
+	}
+	
+	int ret=0;
+	for(int i=0;i<k;++i) ret=max(ret, dp[i]);
+	cout<<ret<<'\n';
+}
+*/
+
+
+// 116ms, 2892KB
 const int MAXN = 3E4 + 1;
 int candy[MAXN], dp[3001];
 pii par[MAXN];

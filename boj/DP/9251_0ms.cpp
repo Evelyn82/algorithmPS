8290042a955099a5b737be1120bdf0527a35b058
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-01-13 17:43:44 +0900

    Update 9251_0ms.cpp

--- boj/DP/9251_0ms.cpp
+++ boj/DP/9251_0ms.cpp
@@ -13,7 +13,7 @@ int main(){
     int j, idx=0;
     for(int i=1;a[i];++i){
         for(j=1;b[j];++j){
-            if(a[i]==b[j]) d[idx][j]=d[idx^1][j-1]+1;
+            if(a[i]==b[j]) d[idx][j]=d[idx^1][j-1]+1;  // 더 정확히는 d[idx][j]=max(d[idx][j], d[idx^1][j-1]+1); 순서대로 누적되니깐 상관없다..?
             else d[idx][j]=max(d[idx][j-1], d[idx^1][j]);
         }
         idx^=1;

4dc06f9c417b751e97a12bbef3b60c47c089e4bb
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-01-16 19:44:57 +0900

    Create 2553.cpp

---
+++ boj/Mathematics/2553.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef long long ll;
+const ll DIV=1e10;
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+	int n; cin>>n;
+	
+	ll ret=1;
+	for(int i=2;i<=n;++i){
+		ret*=i;
+		ret%=DIV;
+		while(ret%10==0) ret/=10;
+	}cout<<ret%10<<'\n';
+}

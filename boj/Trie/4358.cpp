659d77db045eb4a7451884b1f032aae16c16d662
evelyn <54436228+evelyn82ny@users.noreply.github.com>
2021-01-21 10:46:03 +0900

    Create 4358.cpp

---
+++ boj/Trie/4358.cpp
@@ -0,0 +1,62 @@
+// 88ms
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define ALPHA 128
+int sz;
+struct Node{
+    vector<Node*> child;
+    bool terminal;
+    double cnt;
+    Node() : terminal(false), cnt(0.0){
+        child.resize(ALPHA, NULL);
+    }
+    ~Node(){
+        for(int i=0;i<ALPHA;++i){
+            if(child[i]) delete child[i];
+        }
+    }
+    
+    void insert(const char* key){
+        if(*key==0) {
+            terminal=true;
+            cnt++;
+            return;
+        }
+        int next=(int)*key;
+        if(child[next]==NULL) child[next]=new Node();
+        child[next]->insert(key+1);
+    }
+    
+    void showRate(string s){
+        if(terminal){
+            cout<<s<<' '<<(cnt/sz)*100<<'\n';
+        }
+        for(int i=0;i<ALPHA;++i){
+            if(child[i]!=NULL){
+                child[i]->showRate(s+char(i));
+            }
+        }
+    }
+};
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    cout.precision(4);
+    cout.setf(ios::fixed|ios::showpoint);
+    //cout<<fixed;
+    
+    Node* node=new Node();
+    string s;
+    while(getline(cin, s)){
+        if(s.length()==0) break;
+        node->insert(s.c_str());
+        sz++;
+    }
+    node->showRate("");
+}

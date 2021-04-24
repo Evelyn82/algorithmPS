26062b636e4291a361a4ff89131123c0b9c99a9e
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-04-24 11:37:19 +0900

    Update Trio median.cpp

--- open contest/Monthly Code Challenge Season1/Trio median.cpp
+++ open contest/Monthly Code Challenge Season1/Trio median.cpp
@@ -1,69 +1,53 @@
 #include <string>
-#include <vector>
 #include <queue>
 #include <memory.h>
-#include <algorithm>
+#include <vector>
+#define mp make_pair
 using namespace std;
-using vpii = vector<pair<int, int>>;
-using pii = pair<int, int>;
+using pii=pair<int,int>;
+using vpii=vector<pii>;
 
-const int MAXN = 250001;
-struct Node {
-    int n;
-    vector<Node*> child;
-
-    void setChild(Node* other) {
-        child.push_back(other);
-    }
-};
-bool visited[MAXN];
-vpii search(int n, Node* v) {
-    memset(visited, false, sizeof(visited));
-
-    queue<pii> q;
+vector<vector<int>> adj;
+vector<bool> vst;
+vpii findFarthestNode(int n){
+    for(int i=0;i<vst.size();++i) vst[i]=false;
+    vst[n]=true;
+    
     vpii ret;
-    q.push({ n,0 });
-    visited[n] = true;
-    while (!q.empty()) {
-        pii now = q.front();
-        q.pop();
-
-        for (Node* node : v[now.first].child) {
-            int next = node->n;
-            int nextCost = now.second + 1;
-            if (visited[next]) continue;
-            while (!ret.empty() && ret.back().second < nextCost) ret.pop_back();
-
-            visited[next] = true;
-            ret.push_back({ next, nextCost });
-            q.push({ next, nextCost });
+    queue<pii> q;
+    q.push(mp(n, 0));
+    
+    while(!q.empty()){
+        pii now=q.front(); q.pop();
+        
+        for(int i=0;i<adj[now.first].size();++i){
+            int nxt=adj[now.first][i];
+            if(vst[nxt]) continue;
+            vst[nxt]=true;
+            int nxtCost=now.second+1;
+          
+            while(!ret.empty() && ret.back().second<nxtCost) ret.pop_back();
+            ret.push_back(mp(nxt, nxtCost));
+            q.push(mp(nxt, nxtCost));
         }
-    }return ret;
-}
-int solution(int n, vector<vector<int>> edges) {
-
-    sort(edges.begin(), edges.end());
-    Node* v = new Node[n + 1];
-    for (int i = 1; i <= n; ++i) v[i].n = i;
-    for (vector<int> n : edges) {
-        int n1 = n[0], n2 = n[1];
-        v[n1].setChild(&v[n2]);
-        v[n2].setChild(&v[n1]);
     }
+    return ret;
+}
 
-    // 임의의 노드에서 가장 먼 A노를 찾는다.
-    vpii Anode = search(1, v);
-
-    // A노드를 기준으로 B후보를 찾는다
-    vpii Bnode = search(Anode[0].first, v);
-    if (Bnode.size() > 1) return Bnode[0].second;
-
-    // B기준으로 C를 찾는다
-    vpii Cnode = search(Bnode[0].first, v);
+int solution(int n, vector<vector<int>> edges) {
+    adj.resize(n+1);
+    vst.resize(n+1);
+    for(vector<int> v : edges){
+        adj[v[0]].push_back(v[1]);
+        adj[v[1]].push_back(v[0]);
+    }
     
-    // c가 여러개이면 return 지름
-    if (Cnode.size() > 1) return Cnode[0].second;
+    vpii candidate1=findFarthestNode(1);
     
-    // 1개이면 일직선이므로 지름 -1
-    else return Cnode[0].second - 1;
+    vpii candidate2=findFarthestNode(candidate1[0].first);
+    if(candidate2.size()>1) return candidate2[0].second;
+    
+    vpii candidate3=findFarthestNode(candidate2[0].first);
+    if(candidate3.size()>1) return candidate3[0].second;
+    return candidate3[0].second-1;
 }

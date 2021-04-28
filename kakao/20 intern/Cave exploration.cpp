98d1899a78564a6db03754c8e01ac65a5ec8c8b0
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-04-28 15:15:10 +0900

    Update Cave exploration.cpp

--- kakao/20 intern/Cave exploration.cpp
+++ kakao/20 intern/Cave exploration.cpp
@@ -1,63 +1,43 @@
-#include <queue>
 #include <string>
 #include <vector>
-#include <memory.h>
-#include <algorithm>
+#define mp make_pair
 using namespace std;
+vector<vector<pair<int, bool>>> adj;
 
-vector<vector<int>> graph, tree;
-vector<bool> vst, nowvst;
-void makeTree(){
-    queue<int> q;
-    q.push(0);
-    vst[0]=true;
-    
-    while(!q.empty()){
-        int now=q.front(); q.pop();
-        for(int i=0;i<graph[now].size();++i){
-            int nxt=graph[now][i];
-            if(!vst[nxt]){
-                tree[now].push_back(nxt);
-                q.push(nxt);
-                vst[now]=true;
-            }
-        }
+vector<bool> vst, nowPath;
+void oneWay(int n){
+    vst[n]=true;
+    for(pair<int, bool>& next : adj[n]){
+        if(vst[next.first]) next.second=false;
+        else oneWay(next.first);
     }
 }
-bool findCycle(int n){
-    nowvst[n]=true;
+bool isCycle(int n){
     vst[n]=true;
-    
-    for(int i=0;i<tree[n].size();++i){
-        int nxt=tree[n][i];
-        if(!vst[nxt]){
-            if(findCycle(nxt)) return true;
+    nowPath[n]=true;
+    for(pair<int, bool>& next : adj[n]){
+        if(!next.second) continue;
+        if(!vst[next.first]) {
+            if(isCycle(next.first)) return true;
         }
-        if(nowvst[nxt]) return true;
+        if(nowPath[next.first]) return true;
     }
-    nowvst[n]=false;
+    nowPath[n]=false;
     return false;
 }
-
 bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
-    graph.resize(n);
-    tree.resize(n);
-    
-    int from, to;
-    for(vector<int>& v : path){
-        from=v[0]; to=v[1];
-        graph[from].push_back(to);
-        graph[to].push_back(from);
+    adj.resize(n);
+    for(vector<int>& v:path){
+        int from=v[0], to=v[1];
+        adj[from].push_back(mp(to, true));
+        adj[to].push_back(mp(from, true));
     }
     vst.resize(n, false);
-    makeTree();
+    oneWay(0);
     
-    for(vector<int>& v : order){
-        if(v[1]==0) return false;
-        tree[v[0]].push_back(v[1]);
-    }
+    for(vector<int>& v:order) adj[v[0]].push_back(mp(v[1], true));
     
-    for(int i=0;i<n;++i) vst[i]=false;
-    nowvst.resize(n, false);
-    return !findCycle(0);
+    for(int i=0;i<vst.size();++i) vst[i]=false;
+    nowPath.resize(n, false);
+    return !isCycle(0);
 }

b331f8c3c5375480e437003e457e1dc50d70bd11
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-05-01 13:23:31 +0900

    Update candidate key.cpp

--- kakao/19 blind/candidate key.cpp
+++ kakao/19 blind/candidate key.cpp
@@ -1,50 +1,37 @@
 #include <string>
 #include <vector>
 #include <set>
-#include <map>
 #include <algorithm>
 using namespace std;
 
-set<string> st[8];
-map<string, int> mp[8];
-set<int> superKey;
-void getSuperKey(int idx, vector<int> ans, int key, const vector<vector<string>>& relation){
+vector<int> candidateKey;
+set<string> uniqueCnt;
+void getSuperKey(int idx, int key, vector<string> ans, const vector<vector<string>>& relation){
     if(idx==relation[0].size()) return;
     
-    getSuperKey(idx+1, ans, key, relation);
+    getSuperKey(idx+1, key, ans, relation);
     
-    if(st[idx].size()==relation.size()) superKey.insert(1<<idx);
-    else {
-        set<int> s;
-        for(int i=0;i<relation.size();++i){
-            int num=mp[idx][relation[i][idx]];
-            ans[i]=(ans[i] | (1<<num));
-            s.insert(ans[i]);
-        }
-        if(s.size()==relation.size()) superKey.insert(key|(1<<idx));
-        else getSuperKey(idx+1, ans, key|(1<<idx), relation);
+    uniqueCnt.clear();
+    for(int i=0;i<relation.size();++i){
+        ans[i]+=relation[i][idx];
+        uniqueCnt.insert(ans[i]);
     }
+    if(uniqueCnt.size()==relation.size()) candidateKey.push_back(key|(1<<idx));
+    getSuperKey(idx+1, key|(1<<idx), ans, relation);
+    
 }
-void getCandidateKey(){
-    set<int>::iterator iter1, iter2;
-    for(iter1=superKey.begin();iter1!=superKey.end();++iter1){
-        iter2=superKey.end();
-        for(--iter2;*iter1!=*iter2;--iter2){
-            int num=*iter1 & *iter2;
-            if(num==*iter1) superKey.erase(*iter2);
+void getCandidatekey(){
+    sort(candidateKey.begin(), candidateKey.end());
+    for(int i=0;i<candidateKey.size();++i){
+        for(int j=(int)candidateKey.size()-1;j>i;--j){
+            int num=candidateKey[i] & candidateKey[j];
+            if(num==candidateKey[i]) candidateKey.erase(candidateKey.begin()+j);
         }
     }
 }
 int solution(vector<vector<string>> relation) {
-    for(int j=0;j<relation[0].size();++j){
-        for(int i=0;i<relation.size();++i){
-            string s=relation[i][j];
-            st[j].insert(s);
-            if(mp[j].find(s)==mp[j].end()) mp[j][s]=mp[j].size()+1;
-        }
-    }
-    vector<int> ret(relation.size(), 0);
-    getSuperKey(0, ret, 0, relation);
-    getCandidateKey();
-    return superKey.size();
+    vector<string> v(relation.size(), "");
+    getSuperKey(0, 0, v, relation);
+    getCandidatekey();
+    return candidateKey.size();
 }

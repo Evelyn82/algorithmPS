2e37b3b52c3a0390727afb3f002ed4df7093c2ff
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-05-03 11:36:55 +0900

    Update equation.cpp

--- kakao/19 intern/equation.cpp
+++ kakao/19 intern/equation.cpp
@@ -1,57 +1,45 @@
 #include <string>
 #include <vector>
-#include <sstream>
-#include <set>
 #include <cmath>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 
-vector<long long> num;
-vector<char> sign;
-long long findAbsNum(){
-    long long answer=0;
+vector<long long> num, num2;
+vector<char> sign, sign2;
+long long calculate(){
     vector<char> v={'*','+','-'};
-    
-    vector<char> sign2, nextSign;
-    vector<long long> num2, nextNum;
-    
+    long long answer=0;
     do{
-        num2=num;
-        sign2=sign;
-        for(int i=0;i<3;++i){
-            nextSign.clear(); nextNum.clear();
-            nextNum.push_back(num2[0]);
-            for(int j=0;j<sign2.size();j++){
-                if(v[i]!=sign2[j]){
-                    nextSign.push_back(sign2[j]);
-                    nextNum.push_back(num2[j+1]);
-                }
+        num2=num; sign2=sign;
+        for(int i=0;i<v.size();++i){
+            for(int j=0;j<sign2.size();){
+                if(v[i]!=sign2[j]) ++j;
                 else{
-                    if(v[i]=='+') nextNum[nextNum.size()-1]=nextNum.back()+num2[j+1];
-                    else if(v[i]=='-') nextNum[nextNum.size()-1]=nextNum.back()-num2[j+1];
-                    else nextNum[nextNum.size()-1]=nextNum.back()*num2[j+1];
+                    if(v[i]=='+') num2[j]+=num2[j+1];
+                    else if(v[i]=='-') num2[j]-=num2[j+1];
+                    else num2[j]*=num2[j+1];
+                    
+                    sign2.erase(sign2.begin()+j);
+                    num2.erase(num2.begin()+j+1);
                 }
             }
-            num2.clear(); num2=nextNum;
-            sign2.clear(); sign2=nextSign;
         }
         if(abs(num2[0])>answer) answer=abs(num2[0]);
     }while(next_permutation(v.begin(), v.end()));
-    
     return answer;
 }
 void splitted(string expression){
-stringstream ss(expression);
+    stringstream ss(expression);
     long long n; char c;
     ss>>n;
     num.push_back(n);
-    while(ss>>c>>n){
+    while(ss>>c>>n) {
         sign.push_back(c);
         num.push_back(n);
     }
-    
 }
 long long solution(string expression) {
     splitted(expression);
-    return findAbsNum();
+    return calculate();
 }

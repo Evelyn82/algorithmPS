75a457c3216fe0594d49b56a55e4958066839e98
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-05-05 19:42:28 +0900

    Update hotel room assignment.cpp

--- kakao/19 intern/hotel room assignment.cpp
+++ kakao/19 intern/hotel room assignment.cpp
@@ -1,21 +1,21 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 using namespace std;
 using ll=long long;
-map<ll,ll> mp;
+unordered_map<ll,ll> ump;
 
 ll findRoom(ll n){
-    if(mp.find(n)==mp.end()) return n;
-    return mp[n]=findRoom(mp[n]);
+    if(ump.find(n)==ump.end()) return n;
+    return mp[n]=findRoom(ump[n]);
 }
 vector<long long> solution(long long k, vector<long long> room_number) {
     vector<long long> answer;
     for(ll& i : room_number){
         ll room=findRoom(i);
         answer.push_back(room);
-        mp[room]=room+1;
+        ump[room]=room+1;
     }
     return answer;
 }

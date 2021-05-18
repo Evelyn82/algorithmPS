6ecd038b9d9c33847100e320e83385a9cd1abbdf
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-05-18 10:18:49 +0900

    Create 2517.cpp

---
+++ Tree/2517.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+using pii=pair<int, int>;
+
+vector<int> fw;
+int query(int idx){
+    int cnt=0;
+    while(idx){
+        cnt+=fw[idx];
+        idx-=(idx & -idx);
+    }return cnt;
+}
+void add(int idx){
+    while(idx<fw.size()){
+        fw[idx]++;
+        idx+=(idx& -idx);
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    
+    int N; cin>>N;
+    vector<pii> v(N);
+    for(int i=0;i<N;++i){
+        cin>>v[i].second;
+        v[i].first=i;
+    }
+    sort(v.begin(), v.end(), [&](const pii& a, const pii& b){
+        return a.second<b.second;
+    });
+    
+    for(int i=1;i<=N;++i) v[i-1].second=i;
+    sort(v.begin(), v.end(), [&](const pii& a, const pii& b){
+        return a.first<b.first;
+    });
+    
+    fw.resize(N+1,0);
+    int cnt=0;
+    for(pii& p : v){
+        cout<<cnt-query(p.second)+1<<'\n';
+        add(p.second);
+        cnt++;
+    }
+}

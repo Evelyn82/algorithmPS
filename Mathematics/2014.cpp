f00f2097d7671b066833a35db8755b27499c04f6
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-05-23 16:13:58 +0900

    Create 2014.cpp

---
+++ Mathematics/2014.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+using ll=long long;
+
+ll prime[100];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int K,N; cin>>K>>N;
+    priority_queue<ll> pq;
+    for(int i=0;i<K;++i) {
+        cin>>prime[i];
+        pq.push(-prime[i]);
+    }
+    
+    for(int i=0;i<N-1;++i){
+        ll now=-pq.top();
+        pq.pop();
+        
+        for(int j=0;j<K;++j){
+            ll num=now*prime[j];
+            pq.push(-num);
+            
+            if(now % prime[j] == 0) break;
+        }
+    }
+    cout<<-pq.top()<<'\n';
+}

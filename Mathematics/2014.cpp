c582e5f766b40daf4f92f0f640531e07c962da3d
Evelyn <54436228+evelyn82@users.noreply.github.com>
2021-05-23 16:29:20 +0900

    Update 2014.cpp

--- Mathematics/2014.cpp
+++ Mathematics/2014.cpp
@@ -29,3 +29,44 @@ int main(){
     }
     cout<<-pq.top()<<'\n';
 }
+
+// using set
+// 280ms
+/*
+ll prime[100];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    int K,N; cin>>K>>N;
+    
+    set<ll> s;
+    priority_queue<ll> pq;
+    ll mx=0;
+    for(int i=0;i<K;++i) {
+        cin>>prime[i];
+        s.insert(prime[i]);
+        pq.push(-prime[i]);
+        mx=max(mx, prime[i]);
+    }
+    
+    for(int i=0;i<N-1;++i){
+        ll now=-pq.top();
+        pq.pop();
+        
+        
+        for(int j=0;j<K;++j){
+            ll num=now*prime[j];
+            
+            if(pq.size() > N-i && num>=mx) continue;
+            
+            if(s.find(num)==s.end()){
+                pq.push(-num);
+                s.insert(num);
+                mx=max(mx, num);
+            }
+        }
+    }
+    cout<<-pq.top()<<'\n';
+}
+
+*/
